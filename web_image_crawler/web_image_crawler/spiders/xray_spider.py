cc55618d722afd607fce42dec788a52384732b41
vvquocanh <59216532+vvquocanh@users.noreply.github.com>
2020-11-30 15:32:29 +0700

    Crawler 2.0

--- web_image_crawler/web_image_crawler/spiders/xray_spider.py
+++ web_image_crawler/web_image_crawler/spiders/xray_spider.py
@@ -1,12 +1,17 @@
 import scrapy
-
+page = 2
 class WebImagesSpider(scrapy.Spider):
     name = "xray_image"
     start_urls = ["https://www.shutterstock.com/search/healthy+lungs+xray"]
-
     def parse(self, response):
+        global page
         for image in response.css("div.z_h_b900b"):
-            yield {
+            if (image.css("a.z_h_81637 img::attr(src)").get() is not None):
+                 yield {
                 "link": image.css("a.z_h_81637 img::attr(src)").get(),
                 "title": image.css("a.z_h_81637 img::attr(alt)").get(),
-            }
+        }
+        next_page = f"https://www.shutterstock.com/search/healthy+lungs+xray?page={page}" 
+        page += 1
+        if page <= 10:
+            yield response.follow(next_page, callback=self.parse)
