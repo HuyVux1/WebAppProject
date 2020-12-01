3bee3743cab9f1b36f8979abbffd4467c50b4289
Meredith Jason <nhatanhnguyenbui@gmail.com>
2020-12-01 16:44:47 +0700

    web interface

--- monitor_image/script.js
+++ monitor_image/script.js
@@ -1,29 +1,25 @@
-var page = 0;
+counted = 0;
 
 function load() {
-    if (page === 0) {
-        for (var i = 0; i < 20; i++) {
-            document.getElementById(i.toString()).src = "images/image_" + i;
-        }
+    for (var i = 20 * counted; i < 20 * counted + 20; i++) {
+        document.getElementById((i - 20 * counted).toString()).src = "images/image_" + i;
     }
-    if (page < 1) {
-        var x = document.getElementById("back");
-        x.style.visibility = "hidden";
+}
+
+function upward() {
+    counted += 1;
+    for (var i = 20 * counted; i < 20 * counted + 20; i++) {
+        document.getElementsByClassName("gallery-inner").innerHTML = " <div class='gallery-item'> <img id='" + i + "' src=' '></div>"
     }
-    document.getElementById("back").addEventListener("click", function() {
-        page += 1;
-        for (var i = 20 * page; i < 20 * page; i++) {
-            var j = i - 20 * page;
-            document.getElementById(j.toString()).src = "images/image_" + i;
-        }
-    });
-    document.getElementById("next").addEventListener("click", function() {
-        page -= 1;
-        for (var i = 20 * page; i < 20 * page; i++) {
-            var j = i - 20 * page;
-            document.getElementById(j.toString()).src = "images/image_" + i;
-        }
-    });
+    load();
+}
 
-
+function backward() {
+    counted -= 1;
+    if (counted < 0)
+        counted += 1;
+    for (var i = 20 * counted; i < 20 * counted + 20; i++) {
+        document.getElementsByClassName("gallery-inner").innerHTML = " <div class='gallery-item'> <img id='" + i + "' src=' '></div>"
+    }
+    load();
 }
