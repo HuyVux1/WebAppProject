7d2ac7978b49826580a924fe8a4c9fd0e0a039f4
lethuckien <59216515+lethuckien@users.noreply.github.com>
2020-12-02 17:24:52 +0700

    Add files via upload

---
+++ web_image_crawler/web_image_crawler/BeautifulSoupCrawler/xray-crawler.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup as soup 
+import csv
+
+base_url = 'https://www.kenhub.com/en/library/anatomy/normal-chest-x-ray'
+r = requests.get(base_url).text
+page_soup = soup(r, 'lxml')
+
+article = page_soup.article
+
+csv_file = open('xray.csv', 'w')
+csv_writer= csv.writer(csv_file)
+csv_writer.writerow(['image source', 'image title'])
+
+for image in article.find_all('img'):
+    img_src = image['src']
+    img_cap = image['alt'].split(';')[0]
+    csv_writer.writerow([img_src, img_cap])
+
+csv_file.close()
+
